682e6c7ac1a6b3d38f04d3c8910d357eeb00d88a
葛斌 <gbvaq@foxmail.com>
2018-12-01 19:09:41 +0800

    end of section 6 添加第三方js css组件

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,28 +10,33 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       next => {
+        this.alertify.success('登陆成功');
         console.log('登陆成功');
       },
       error => {
+        this.alertify.error(error);
         console.log(error);
       }
     );
   }
 
   loggenIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggendIn();
   }
 
   logout() {
     localStorage.removeItem('token');
+    this.alertify.message('登出');
     console.log('登出');
   }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,16 +12,21 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   register() {
     this.authService.register(this.model).subscribe(
       () => {
+        this.alertify.success('注册成功');
         console.log('注册成功');
       },
       error => {
+        this.alertify.error(error);
         console.log(error);
       }
     );
@@ -28,6 +34,5 @@ export class RegisterComponent implements OnInit {
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('取消');
   }
 }
