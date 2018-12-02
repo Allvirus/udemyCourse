17bb06b8eb7ce980816705c0cc98cbc9d2c5559b
葛斌 <gbvaq@foxmail.com>
2018-12-03 01:19:11 +0800

    end of section 8

---
+++ DatingApp.API/Helpers/AutoMapeerProfiles.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using DatingApp.API.Model;
+using DatingApp.API.Dtos;
+using System.Linq;
+
+namespace DatingApp.API.Helpers
+{
+    //预设AutoMapper映射类
+    public class AutoMapeerProfiles : Profile
+    {
+        public AutoMapeerProfiles()
+        {
+
+            //配置photo Url映射
+            CreateMap<User, UserForListDto>()
+            .ForMember(dest => dest.PhotoUrl, opt =>
+            {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(dest => dest.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+            CreateMap<User, UserForDetailedDto>()
+                        .ForMember(dest => dest.PhotoUrl, opt =>
+            {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(dest => dest.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+
+            CreateMap<Photo, PhotoForDetailDto>();
+        }
+    }
+}
