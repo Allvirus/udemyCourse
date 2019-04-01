31d7a6e1d178fc9f224f12e3b754fa3a2872acbc
Allvirus <gbvaq@foxmail.com>
2019-04-01 19:06:26 +0800

    API message send test

--- DatingApp.API/Helpers/AutoMapeerProfiles.cs
+++ DatingApp.API/Helpers/AutoMapeerProfiles.cs
@@ -22,7 +22,7 @@ namespace DatingApp.API.Helpers
                 opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
             });
             CreateMap<User, UserForDetailedDto>()
-                        .ForMember(dest => dest.PhotoUrl, opt =>
+                .ForMember(dest => dest.PhotoUrl, opt =>
             {
                 opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
             })
@@ -39,6 +39,14 @@ namespace DatingApp.API.Helpers
             CreateMap<PhotoForCreationDto, Photo>();
 
             CreateMap<UserForRegisterDto, User>();
+
+            CreateMap<MessageForCreationDto,Message>().ReverseMap();
+
+            CreateMap<Message,MessageToReturnDto>()
+                .ForMember(m => m.SenderPhotoUrl,opt => opt
+                    .MapFrom(u => u.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(m => m.RecipientPhotoUrl,opt => opt
+                    .MapFrom(u => u.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
         }
     }
 }
